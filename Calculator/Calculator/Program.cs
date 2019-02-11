154cfd4accccac9d52c3d398f58e9c633e82a840
shussain03 <46450877+shussain03@users.noreply.github.com>
2019-02-11 17:35:59 +0000

    calculator adds

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Convert.ToInt32(args[0]) + Convert.ToInt32(args[1]));
+           
+        }
+    }
+}
